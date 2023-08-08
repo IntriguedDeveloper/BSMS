de767e81836ebeb617ce9d2eaae2fc2f618ba7cd
Ankit Raj <ruby08241@gmail.com>
2023-08-08 16:39:06 +0530

    Added HomePage

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,41 @@
 import ClientAuthPage from "./Auth/ClientAuthPage.jsx";
-import React from "react";
+import HomePage from "./Auth/HomePage.jsx";
+import './LoadingBar.css';
+import React, { useEffect, useState} from "react";
+import { initializeApp } from "firebase/app";
+import { onAuthStateChanged, getAuth } from "firebase/auth";
+import { firebaseConfig } from "./firebase.js";
+import { BrowserRouter as Router, Routes, Route , useNavigate} from "react-router-dom";
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
 export default function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setIsLoggedIn(true);
+      } else {
+        setIsLoggedIn(false);
+      }
+      setIsLoading(false);
+    });
+
+    return () => unsubscribe(); // Clean up the auth state listener
+  }, [auth]);
+  if(isLoading){
+    return(
+      <div className="loading-spinner-container">
+        <div className="loading-spinner"></div>
+      </div>
+    )
+  }
   return (
-    <>
-      <ClientAuthPage></ClientAuthPage>
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element = {isLoggedIn ? <HomePage></HomePage> : <ClientAuthPage></ClientAuthPage>}></Route>
+      </Routes>
+    </Router>
   );
 }
--- src/Auth/ClientAuthPage.jsx
+++ src/Auth/ClientAuthPage.jsx
@@ -1,4 +1,5 @@
 import { initializeApp } from "firebase/app";
+import { firebaseConfig } from "../firebase";
 import {
   getAuth,
   createUserWithEmailAndPassword,
@@ -6,15 +7,7 @@ import {
 } from "firebase/auth";
 import "./ClientAuthPage.css";
 import { useState } from "react";
-const firebaseConfig = {
-  apiKey: "AIzaSyCRnopWA6aEvdLIVFYs9Cl186Lce0Js3lk",
-  authDomain: "biometric-student-manage-ca3c0.firebaseapp.com",
-  projectId: "biometric-student-manage-ca3c0",
-  storageBucket: "biometric-student-manage-ca3c0.appspot.com",
-  messagingSenderId: "825576257825",
-  appId: "1:825576257825:web:1df2e4ca6db95f4fdd2660",
-  measurementId: "G-L9K6VXVC3Z",
-};
+
 const app = initializeApp(firebaseConfig);
 
 export default function ClientAuthPage() {
