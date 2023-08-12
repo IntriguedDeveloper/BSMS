8755f1b6340a85e7ca8fb121082f397759e60175
Ankit Raj <ruby08241@gmail.com>
2023-08-12 16:21:09 +0530

    minor changes

--- src/Home/AttendancePage.jsx
+++ src/Home/AttendancePage.jsx
@@ -5,20 +5,20 @@ export default function AttendancePage() {
   return (
     <>
       <div className="navContainer">
-        <nav class="datebar">
+        <nav className="datebar">
           <div className="optionWrapper">
             <input type="number" className="dateInput"></input>
-            <select class="date-dropdown" id="day"></select>
+            <select className="date-dropdown" id="day"></select>
           </div>
           <div className="optionWrapper">
             <input type="text" className="monthInput"></input>
-            <select class="date-dropdown" id="month">
+            <select className="date-dropdown" id="month">
               <option value="">Month</option>
             </select>
           </div>
           <div className="optionWrapper">
             <input className="yearInput" type="number"></input>
-            <select class="date-dropdown" id="year">
+            <select className="date-dropdown" id="year">
               <option value="">Year</option>
             </select>
           </div>
