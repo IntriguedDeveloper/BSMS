fa2e108d4813a6d2bed818e5b76885299474896e
Ankit Raj <ruby08241@gmail.com>
2023-08-12 16:26:42 +0530

    resolved text field and dropdown

--- src/Home/AttendancePage.jsx
+++ src/Home/AttendancePage.jsx
@@ -1,25 +1,56 @@
 import React from "react";
 import "./AttendancePage.css";
-import checkIcon from '../assets/check-1.png';
+import checkIcon from "../assets/check-1.png";
+
 export default function AttendancePage() {
+  // Function to generate options for dropdowns
+  const generateOptions = (start, end) => {
+    const options = [];
+    for (let i = start; i <= end; i++) {
+      options.push(
+        <option key={i} value={i}>
+          {i}
+        </option>
+      );
+    }
+    return options;
+  };
+
   return (
     <>
       <div className="navContainer">
         <nav className="datebar">
           <div className="optionWrapper">
-            <input type="number" className="dateInput"></input>
-            <select className="date-dropdown" id="day"></select>
-          </div>
-          <div className="optionWrapper">
-            <input type="text" className="monthInput"></input>
-            <select className="date-dropdown" id="month">
-              <option value="">Month</option>
+            <input
+              type="number"
+              className="dateInput"
+              placeholder="Day"
+            ></input>
+            <select className="date-dropdown" id="day">
+              <option value="">Day</option>
+              {generateOptions(1, 31)}
             </select>
           </div>
           <div className="optionWrapper">
-            <input className="yearInput" type="number"></input>
+            <input
+              type="text"
+              className="monthInput"
+              placeholder="Month"
+            ></input>
+            <select className="date-dropdown" id="month">
+              <option value="">Month</option>
+              {generateOptions(1, 12)}
+            </select>
+          </div>
+          <div className="optionWrapper">
+            <input
+              className="yearInput"
+              type="number"
+              placeholder="Year"
+            ></input>
             <select className="date-dropdown" id="year">
               <option value="">Year</option>
+              {generateOptions(2020, 2030)}
             </select>
           </div>
         </nav>
@@ -28,44 +59,18 @@ export default function AttendancePage() {
             <div className="column">1.</div>
             <div className="column">Ankit Raj</div>
             <div className="column">
-              <img src = {checkIcon} className = "statusIcon"></img>
+              <img src={checkIcon} className="statusIcon" alt="Check Icon" />
             </div>
           </div>
+          {/* Add more student rows */}
           <div className="row">
-            <div className="column">1.</div>
-            <div className="column">Ankit Raj</div>
+            <div className="column">2.</div>
+            <div className="column">Jane Smith</div>
             <div className="column">
-              <img src = {checkIcon} className = "statusIcon"></img>
+              <img src={checkIcon} className="statusIcon" alt="Check Icon" />
             </div>
           </div>
-          <div className="row">
-            <div className="column">1.</div>
-            <div className="column">Ankit Raj</div>
-            <div className="column">
-              <img src = {checkIcon} className = "statusIcon"></img>
-            </div>
-          </div>
-          <div className="row">
-            <div className="column">1.</div>
-            <div className="column">Ankit Raj</div>
-            <div className="column">
-              <img src = {checkIcon} className = "statusIcon"></img>
-            </div>
-          </div>
-          <div className="row">
-            <div className="column">1.</div>
-            <div className="column">Ankit Raj</div>
-            <div className="column">
-              <img src = {checkIcon} className = "statusIcon"></img>
-            </div>
-          </div>
-          <div className="row">
-            <div className="column">1.</div>
-            <div className="column">Ankit Raj</div>
-            <div className="column">
-              <img src = {checkIcon} className = "statusIcon"></img>
-            </div>
-          </div>
+          {/* Add more student rows */}
         </div>
       </div>
     </>
