dd4e09386f0913508667dd4d216f53adbeef6132
BinaryBlossom <125644392+JiviDada@users.noreply.github.com>
2023-08-12 16:17:01 +0530

    Nav completed, grids  still on work

--- src/Home/AttendancePage.jsx
+++ src/Home/AttendancePage.jsx
@@ -1,9 +1,73 @@
 import React from "react";
 import "./AttendancePage.css";
-export default function AttendancePage(){
-    return (
-        <>
-            <h1>Hello from AttendancePage</h1>
-        </>
-    )
+import checkIcon from '../assets/check-1.png';
+export default function AttendancePage() {
+  return (
+    <>
+      <div className="navContainer">
+        <nav class="datebar">
+          <div className="optionWrapper">
+            <input type="number" className="dateInput"></input>
+            <select class="date-dropdown" id="day"></select>
+          </div>
+          <div className="optionWrapper">
+            <input type="text" className="monthInput"></input>
+            <select class="date-dropdown" id="month">
+              <option value="">Month</option>
+            </select>
+          </div>
+          <div className="optionWrapper">
+            <input className="yearInput" type="number"></input>
+            <select class="date-dropdown" id="year">
+              <option value="">Year</option>
+            </select>
+          </div>
+        </nav>
+        <div className="primaryGrid">
+          <div className="row">
+            <div className="column">1.</div>
+            <div className="column">Ankit Raj</div>
+            <div className="column">
+              <img src = {checkIcon} className = "statusIcon"></img>
+            </div>
+          </div>
+          <div className="row">
+            <div className="column">1.</div>
+            <div className="column">Ankit Raj</div>
+            <div className="column">
+              <img src = {checkIcon} className = "statusIcon"></img>
+            </div>
+          </div>
+          <div className="row">
+            <div className="column">1.</div>
+            <div className="column">Ankit Raj</div>
+            <div className="column">
+              <img src = {checkIcon} className = "statusIcon"></img>
+            </div>
+          </div>
+          <div className="row">
+            <div className="column">1.</div>
+            <div className="column">Ankit Raj</div>
+            <div className="column">
+              <img src = {checkIcon} className = "statusIcon"></img>
+            </div>
+          </div>
+          <div className="row">
+            <div className="column">1.</div>
+            <div className="column">Ankit Raj</div>
+            <div className="column">
+              <img src = {checkIcon} className = "statusIcon"></img>
+            </div>
+          </div>
+          <div className="row">
+            <div className="column">1.</div>
+            <div className="column">Ankit Raj</div>
+            <div className="column">
+              <img src = {checkIcon} className = "statusIcon"></img>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
