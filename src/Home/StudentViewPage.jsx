f18e4d73da78b87a7b9e33f257ec5d04181ea539
Ankit Raj <ruby08241@gmail.com>
2023-08-13 15:28:20 +0530

    view page auth improvision

--- src/Home/StudentViewPage.jsx
+++ src/Home/StudentViewPage.jsx
@@ -1,8 +1,16 @@
 import React, { useState, useEffect } from "react";
 import "./StudentViewPage.css";
-import { getFirestore, collection, query, getDocs } from "firebase/firestore";
+import {
+  getFirestore,
+  collection,
+  query,
+  getDocs,
+  where,
+} from "firebase/firestore";
+import { getAuth } from "firebase/auth";
 import { AppInstance } from "../firebase";
 const db = getFirestore(AppInstance);
+const auth = getAuth(AppInstance);
 export default function StudentViewPage() {
   const [studentRows, setStudentRows] = useState([]);
 
@@ -10,14 +18,24 @@ export default function StudentViewPage() {
     generateStudentRows();
   }, []);
   const generateStudentRows = async () => {
+    const user = auth.currentUser;
+    const userEmail = user.email;
+    console.log(userEmail);
+    const collection_ref_teachers = collection(db, "Teachers");
+    const classQuery = query(
+      collection_ref_teachers,
+      where("Email", "==", userEmail)
+    );
+    const querySnapShot = await getDocs(classQuery);
+    const className = querySnapShot.docs[0].data().Class;
     const collection_ref = collection(
       db,
       "/classes/class-X/X-A/Students/Information"
     );
     const q = query(collection_ref);
-    const querySnapShot = await getDocs(q);
+    const querySnapShotRows = await getDocs(q);
 
-    const sortedRows = querySnapShot.docs
+    const sortedRows = querySnapShotRows.docs
       .map((doc) => doc.data())
       .sort((a, b) => a.roll - b.roll) // Sort by roll number
       .map((studentData, index) => (
