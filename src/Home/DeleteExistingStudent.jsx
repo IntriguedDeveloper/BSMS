37dc4b64ed94a3348bb35cf9678466469d625ad7
BinaryBlossom <125644392+JiviDada@users.noreply.github.com>
2023-08-13 15:05:56 +0530

    View page done , delte page on progress

--- src/Home/DeleteExistingStudent.jsx
+++ src/Home/DeleteExistingStudent.jsx
@@ -1,3 +1,14 @@
+import React from "react";
 export default function DeleteExistingStudent(){
-
+    return(
+        <>
+            <div className="deletePageContainer">
+                <div className="deletePage">
+                    <input className="nameIn" type="text" placeholder="Name "></input>
+                    <input className="rollIn" placeholder="Roll No. " type="number"></input>
+                    <input className="buttonC"></input>
+                </div>
+            </div>
+        </>
+    )
 }
--- src/Home/StudentViewPage.jsx
+++ src/Home/StudentViewPage.jsx
@@ -1,5 +1,26 @@
-export default function StudentViewPage(){
-    return(
-        <h1> Hello from StudentViewPage</h1>
-    )
+import React from "react";
+import "./StudentViewPage.css";
+export default function StudentViewPage() {
+  return (
+    <>
+      <div className="viewContainer">
+        <div className="primaryGrid">
+        <div className="rowTitle">
+            <div className="column">Roll no.</div>
+            <div className="column">Names</div>
+          </div>
+          <div className="row">
+            <div className="column">1.</div>
+            <div className="column">Ankit Raj</div>
+          </div>
+          {/* Add more student rows */}
+          <div className="row">
+            <div className="column">2.</div>
+            <div className="column">Jane Smith</div>
+          </div>
+          {/* Add more student rows */}
+        </div>
+      </div>
+    </>
+  );
 }
